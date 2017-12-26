1145238836b08fe955f50875b5fa899a28d8d095
philipperemy <premy@reactive.co.jp>
2017-12-26 16:39:45 +0900

    first commit

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@ int main() {
 
 	// vigenere encoding
 
-    std::string msg = "{\"HELLO\"THECRAZYPROGRAMMER;;";
-    std::string key = "HELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLOHELLO";
+    std::string msg = "PHILIPPE123 AND I FEEL DAMN GREAT";
+    std::string key = "HELLOHEL";
     std::string encryptedMsg = encrypt_vigenere(msg, key);
     std::string newKey = extend_key(msg, key);
     std::string decryptedMsg = decrypt_vigenere(encryptedMsg, newKey);
@@ -24,30 +24,7 @@ int main() {
  	std::cout << "New Generated Key: " <<  newKey << std::endl;
  	std::cout << "Encrypted Message: " << encryptedMsg << std::endl;
  	std::cout << "Decrypted Message: " << decryptedMsg << std::endl;
- 	
- 	
- 	// b64 encoding
- 	
- 	std::string msg2 = "hello the world";
- 	std::string encoded64msg2 = base64_encode(msg2);
- 	std::cout << encoded64msg2 << std::endl;
- 	std::string decoded64msg2 = base64_decode(encoded64msg2);
- 	std::cout << decoded64msg2 << std::endl;
- 	
- 	// vigenere and b64 encoding.
- 	// works only for capital letters.
- 	// https://www.thecrazyprogrammer.com/2017/08/vigenere-cipher-c-c.html
- 	// https://stackoverflow.com/questions/342409/how-do-i-base64-encode-decode-in-c
- 	std::string msg3 = "HELLO THE WORLD ITS PHILIPPE;;";
- 	std::cout << "_________________________________________________" << std::endl;
- 	std::cout << msg3 << std::endl;
- 	msg3 = encrypt_vigenere(msg3, key);
- 	std::cout << msg3 << std::endl;
- 	msg3 = base64_encode(msg3);
- 	std::cout << "Message to be sent: " << msg3 << std::endl;
- 	msg3 = base64_decode(msg3);
- 	std::cout << msg3 << std::endl;
- 	std::cout << decrypt_vigenere(msg3, newKey) << std::endl; 	
+
  
     return 0;
 }
--- vigenere.h
+++ vigenere.h
@@ -7,6 +7,19 @@
 
 using namespace std;
 
+std::string AVAILABLE_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
+
+int index(char c) {
+	for(int ii = 0; ii < AVAILABLE_CHARS.size(); ii++) {
+		if(AVAILABLE_CHARS[ii] == c) {
+			std::cout << ii << " " << c << std::endl;
+			return ii;
+		}
+	}
+	return -1;
+}
+
+
 std::string extend_key(std::string& msg, std::string& key) {
 	//generating new key
 	int msgLen = msg.size();
@@ -33,8 +46,8 @@ std::string encrypt_vigenere(std::string& msg, std::string& key) {
     //encryption
     for(i = 0; i < msgLen; ++i) {
     	// std::cout << msg[i] << " " << isalnum(msg[i]) << std::endl;
-    	if(isalpha(msg[i])) {
-    		encryptedMsg[i] = ((msg[i] + newKey[i]) % 26) + 'A';
+    	if(isalnum(msg[i]) or msg[i] == ' ') {
+    		encryptedMsg[i] = AVAILABLE_CHARS[((index(msg[i]) + index(newKey[i])) % AVAILABLE_CHARS.size())];
     	} else {
     		encryptedMsg[i] = msg[i];
     	}
@@ -50,8 +63,8 @@ std::string decrypt_vigenere(std::string& encryptedMsg, std::string& newKey) {
 	std::string decryptedMsg(msgLen, 'x');
 	int i;
     for(i = 0; i < msgLen; ++i) {
-    	if(isalpha(encryptedMsg[i])) {
-    		decryptedMsg[i] = (((encryptedMsg[i] - newKey[i]) + 26) % 26) + 'A';
+    	if(isalnum(encryptedMsg[i]) or encryptedMsg[i] == ' ') {
+    		decryptedMsg[i] = AVAILABLE_CHARS[(((index(encryptedMsg[i]) - index(newKey[i])) + AVAILABLE_CHARS.size()) % AVAILABLE_CHARS.size())];
     	} else {
     		decryptedMsg[i] = encryptedMsg[i];
     	}
